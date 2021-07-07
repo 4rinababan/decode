d837b6324d632877f6d9f1903fc9822162483048
ARI NABABAN <arinababan123@gmail.com>
2021-07-07 13:34:42 +0700

    first commit

---
+++ decode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace decode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte[] data = Convert.FromBase64String("password");
+            //string decodedString = Encoding.Default.GetString(data);
+            string base64Decoded = "password";
+            string base64Encoded;
+            byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(base64Decoded);
+            base64Encoded = System.Convert.ToBase64String(data);
+            Console.WriteLine(base64Encoded);
+            Console.ReadKey();
+        }
+    }
+}
